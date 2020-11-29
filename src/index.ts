d046709536bc67ad05c7ed2d28a34c47c5d126ec
Michael <micjanic20@gmail.com>
2020-11-29 13:35:05 -0600

    micro orm

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    const post = orm.em.create(Post, { title: "first post" });
+    await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+    console.log(err);
+});
