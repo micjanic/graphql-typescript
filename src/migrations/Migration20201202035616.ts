1ce91abe90052d961928733487ee2f42c24f1842
Michael <micjanic20@gmail.com>
2020-12-01 23:19:38 -0600

    register and login validation

---
+++ src/migrations/Migration20201202035616.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20201202035616 extends Migration {
+
+  async up(): Promise<void> {
+
+  }
+
+}
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { Post } from "../entities/Post";
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
 import { MyContext } from "src/types";
 
 @Resolver()
@@ -11,7 +11,7 @@ export class PostResolver {
   }
   @Query(() => Post, { nullable: true })
   post(
-    @Arg('id', () => Int) id: number,
+    @Arg('id') id: number,
     @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { id });
   }
